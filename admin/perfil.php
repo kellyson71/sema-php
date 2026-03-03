c1a22fe5ae69780ed746565de6faa0c66d58c396
Kellyson <kellyson@example.com>
2026-03-03 00:38:43 -0300

    teste

--- admin/perfil.php
+++ admin/perfil.php
@@ -183,7 +183,7 @@ include 'header.php';
         <div class="card">
             <div class="card-header">
                 <i class="fas fa-user-circle me-1"></i>
-                Informações do Perfil
+                Informações do Perfil teste
             </div>
             <div class="card-body text-center">
                 <div class="mb-4">
