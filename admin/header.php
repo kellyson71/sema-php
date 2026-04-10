8dd4417d37efb133980101fe957d6f5da4afba61
Kellyson <kellyson@example.com>
2026-04-10 07:27:26 -0300

    3.2

--- admin/header.php
+++ admin/header.php
@@ -712,7 +712,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 <i class="fab fa-whatsapp"></i>
                 <span>Problemas? Fale conosco</span>
             </a>
-            <span class="version-text">v2.8</span>
+            <span class="version-text">v3.2</span>
         </div>
     </div>
 
